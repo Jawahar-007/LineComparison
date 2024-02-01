3e0e70989244b620e89496a86c09864c51583fdf
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-02-01 17:17:14 +0530

    Comparing Lines Logic

--- src/com/bridgelabz/linecomparison/linecomparison.java
+++ src/com/bridgelabz/linecomparison/linecomparison.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.linecomparison;
 
-public class linecomparision {
+public class linecomparison {
     public static void main(String args[]){
         Point p1 = new Point(1,5);
         Point p2 = new Point(4,5);
@@ -10,9 +10,15 @@ public class linecomparision {
         line l2 = new line(p3,p4);
 
         double len1 = l1.calclenght();
+        System.out.println(len1);
         double len2 = l2.calclenght();
+        System.out.println(len2);
 
-        System.out.println(" Length of Line 1 : "+len1);
-        System.out.println(" Length of Line 2 : "+len2);
+        if(len1 == len2)
+            System.out.println("Lengths are equal :"+len1);
+        else if (len1>len2)
+            System.out.println("\nGreater Length :"+len1);
+        else
+            System.out.println("\n Greater Length :"+len2);
     }
 }
